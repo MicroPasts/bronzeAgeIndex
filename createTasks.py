1ec9e1f67e9826af608bf7b3c194b32196a69955
Daniel Lombraña González <teleyinex@gmail.com>
2014-03-14 01:50:29 +0100

    Add category

--- createTasks.py
+++ createTasks.py
@@ -140,6 +140,7 @@ def run(app_config, options):
     def setup_app():
         app = find_app_by_short_name()
         app.long_description = contents('long_description.html')
+        app.category_id = 2
         app.info['task_presenter'] = contents('template.html')
         app.info['thumbnail'] = app_config['thumbnail']
         app.info['tutorial'] = contents('tutorial.html')
